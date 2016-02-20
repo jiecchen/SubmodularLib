e1bb8aec5ff97dd5b4d9540f3523a414deca928d
jiecchen <jiecchen@indiana.edu>
2016-02-19 22:50:19 -0500

    add deepClone

--- src/main/java/me/xmerge/util/UtilFunctions.java
+++ src/main/java/me/xmerge/util/UtilFunctions.java
@@ -1,5 +1,9 @@
 package me.xmerge.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 /**
@@ -11,6 +15,25 @@ public class UtilFunctions {
     public static<T> void printArrayList (ArrayList<T> arr) {
         for (T tmp: arr) {
             System.out.println(tmp);
+        }
+    }
+
+
+    /**
+     * This method makes a "deep clone" of any Java object it is given.
+     */
+    public static Object deepClone(Object object) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(object);
+            ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            return ois.readObject();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
