4cb929528656ad4ee1fc17c4c5aa444ea3bfe091
jiecchen <jiecchen@indiana.edu>
2016-02-20 01:17:12 -0500

    added

--- src/main/java/me/xmerge/streaming/SieveStream.java
+++ src/main/java/me/xmerge/streaming/SieveStream.java
@@ -1,12 +1,86 @@
 package me.xmerge.streaming;
 
+import me.xmerge.core.SubmodularBuffer;
+import me.xmerge.util.UtilFunctions;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
 /**
  *
+ * Streaming algorithm for maximizing a non-negative monotone submodular function
+ * under cardinality constraint.
  *
- * Reference: Badanidiyuru, Ashwinkumar, et al. "Streaming submodular maximization: Massive data sum
- *            marization on the fly." KDD'14
- * Created by cjc on 2/16/16.
+ * + Author: Jiecao Chen
+ * + Date: 2/16/16
+ *
+ * ## Reference
+ * + Badanidiyuru, Ashwinkumar, et al. "Streaming submodular maximization: Massive data sum
+ *   marization on the fly." KDD'14
+ *
  */
-public class SieveStream {
 
+public class SieveStream<T> implements StreamingAlgorithm<T>{
+    private ArrayDeque<SieveStreamOPT<T>> optValues;
+    private final double eps;
+    private double m; // largest value of func({elem})
+    private final SubmodularBuffer<T> emptyFunc;
+    private final int maxSize;
+    /**
+     *
+     * @param _eps parameter to control the approximation ratio: (1/2 - _eps) to the optimum
+     * @param _emptyFunc an instance of FuncT (Submodular function) with empty buffer
+     */
+    public SieveStream(double _eps, int _maxSize, SubmodularBuffer<T> _emptyFunc) {
+        eps = _eps;
+        optValues = new ArrayDeque<>();
+        m = 0.;
+        emptyFunc = _emptyFunc;
+        maxSize = _maxSize;
+    }
+
+
+
+    public void processItem(T elem) {
+        double mGain = emptyFunc.marginalGain(elem);
+        if (mGain > m)
+            m = mGain;
+        // remove those have OPT < m from optValues
+        while (optValues.size() > 0 && optValues.getFirst().getOPT() < m) {
+            optValues.removeFirst();
+        }
+
+        // add new SieveStreamOPT instance
+        while (optValues.getLast().getOPT() * (1 + eps) <= 2 * maxSize * m) {
+            // create a new SieveStreamOPT instance
+            double newOpt = optValues.getLast().getOPT() * (1 + eps);
+            SubmodularBuffer<T> newFunc = (SubmodularBuffer<T>) UtilFunctions.deepClone(emptyFunc);
+            SieveStreamOPT<T> knowOpt = new SieveStreamOPT<>(newOpt, maxSize, newFunc);
+            // add it to optValues
+            optValues.add(knowOpt);
+
+            // now update each instance in optValues
+            for (SieveStreamOPT<T> ssOpt: optValues) {
+                ssOpt.processItem(elem);
+            }
+        }
+    }
+
+    /**
+     *
+     * @return the best possible solution of the constrained submodular maximization problem
+     */
+    public ArrayList<T> getOptimalSolution() {
+        // find solution with optimal function value
+        Double maxValue = Double.NEGATIVE_INFINITY;
+        ArrayList<T> ans = new ArrayList<>();
+        for (SieveStreamOPT<T> ssOpt: optValues) {
+            if (ssOpt.getFunc().getCurrentValue() > maxValue) {
+                maxValue = ssOpt.getFunc().getCurrentValue();
+                ans = ssOpt.getFunc().getSolution();
+            }
+        }
+
+        return ans;
+    }
 }
