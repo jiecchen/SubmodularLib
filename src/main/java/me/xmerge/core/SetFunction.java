82a58cf8ba9d7e8760b08ee96edb8cb7a2f874a4
jiecchen <jiecchen@indiana.edu>
2016-03-04 01:42:55 -0500

    updated

--- src/main/java/me/xmerge/core/SetFunction.java
+++ src/main/java/me/xmerge/core/SetFunction.java
@@ -1,10 +1,16 @@
 package me.xmerge.core;
 
+import java.util.ArrayList;
 import java.util.Deque;
 
 /**
  * Interface for set function
  */
 public interface SetFunction<T> {
-    double eval(Deque<T> S);
+    /**
+     *
+     * @param A the given set
+     * @return the value of f(A)
+     */
+    double eval(ArrayList<T> A);
 }
