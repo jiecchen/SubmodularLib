1877a94db6ed8aa91578ee10daf83d5215060d9c
jiecchen <jiecchen@indiana.edu>
2016-03-04 01:18:04 -0500

    fix an error

---
+++ src/main/java/me/xmerge/streaming/RandomStream.java
@@ -0,0 +1,7 @@
+package me.xmerge.streaming;
+
+/**
+ * Created by cjc on 3/4/16.
+ */
+public class RandomStream {
+}
---
+++ src/main/java/me/xmerge/streaming/RandomStreamAlpha.java
@@ -0,0 +1,7 @@
+package me.xmerge.streaming;
+
+/**
+ * Created by cjc on 3/3/16.
+ */
+public class RandomStreamAlpha {
+}
