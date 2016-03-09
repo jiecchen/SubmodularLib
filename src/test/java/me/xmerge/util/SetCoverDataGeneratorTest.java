578396abfc4c4f78beb4e1bff867aee61280155a
jiecchen <jiecchen@indiana.edu>
2016-03-08 21:52:27 -0500

    added

---
+++ src/test/java/me/xmerge/util/SetCoverDataGeneratorTest.java
@@ -0,0 +1,22 @@
+package me.xmerge.util;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * Test for SetCoverDataGenerator
+ */
+public class SetCoverDataGeneratorTest extends TestCase {
+
+    public void setUp() {
+    }
+
+    public void test() {
+        ArrayList<Integer> groundSet = IntegersGenerator.range(10);
+        SetCoverDataGenerator<Integer> scGenerator = new SetCoverDataGenerator<>();
+        ArrayList<HashSet<Integer>> sets = scGenerator.generate(5, groundSet);
+        
+    }
+}
