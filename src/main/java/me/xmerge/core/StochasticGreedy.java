28fe59565b8163c6ce408a8a43019d9eb5de5a2d
jiecchen <jiecchen@indiana.edu>
2016-03-10 00:15:49 -0500

    add lazy evaluation

--- src/main/java/me/xmerge/core/StochasticGreedy.java
+++ src/main/java/me/xmerge/core/StochasticGreedy.java
@@ -1,12 +1,14 @@
 package me.xmerge.core;
 
+import me.xmerge.util.Pair;
 import me.xmerge.util.Sample;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.PriorityQueue;
 
 /**
- * Class for Stochastic Greedy algorithm
+ * Class for Stochastic Greedy algorithm with lazy evaluation
  */
 public class StochasticGreedy {
 
@@ -17,26 +19,52 @@ public class StochasticGreedy {
         if (nSamples < 1)
             nSamples = 1;
 
+        // keep the upper bounds
+        ArrayList<Double> upperBoundsFixed = new ArrayList<>();
+        for (int i = 0; i < V.size(); ++i)
+            upperBoundsFixed.add(Double.NEGATIVE_INFINITY);
+
+        int stepCounter = 0;
 
         while (func.size() < maxSize && included.size() < V.size()) {
+            // each loop, one element will be picked
+
+            // filter unmarked elements
             ArrayList<Integer> indices = new ArrayList<>();
             for (int i = 0; i < V.size(); i++)
                 if (!included.contains(i))
                     indices.add(i);
+
+
+            // sample
             indices = Sample.SampleWithoutRplmt(indices, nSamples);
-            int m = -1;
-            double m_gain = -1e200;
-            for (int j : indices) {
-                double tmp = func.marginalGain(V.get(j));
-                if (m == -1 || tmp > m_gain) {
-                    m = j;
-                    m_gain = tmp;
-                }
+
+            // build a heap
+            PriorityQueue<Pair<Integer, Double>> upperBounds = new PriorityQueue<>();
+            for (int idx : indices) {
+                upperBounds.add(new Pair<>(idx, upperBoundsFixed.get(idx)));
             }
-            func.addToSolution(V.get(m));
-            included.add(m);
+
+            // pick the one with largest marginal gain
+            Double tmp = Double.MAX_VALUE;
+            while (tmp > upperBounds.peek().getValue()) {
+                stepCounter++;
+                Pair<Integer, Double> p = upperBounds.poll();
+                tmp = -func.marginalGain(V.get(p.getKey()));
+                // update bound
+                upperBoundsFixed.set(p.getKey(), tmp);
+                upperBounds.add(new Pair<>(p.getKey(), tmp));
+            }
+
+            // the top actually has the largest gain
+            Pair<Integer, Double> p = upperBounds.poll();
+            // add to solution
+            func.addToSolution(V.get(p.getKey()));
+
+            included.add(p.getKey());
         }
 
+        System.out.println("StochGreedy total steps = " + stepCounter);
         return func.getSolution();
     }
 
