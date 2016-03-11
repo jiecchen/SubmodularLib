efb35255fb57a28263e317302507fde4d47a1201
jiecchen <jiecchen@indiana.edu>
2016-03-10 22:38:32 -0500

    updated

--- src/python/greedys.py
+++ src/python/greedys.py
@@ -5,20 +5,20 @@ import StringIO
 import draw
 
 
-nSets = 500
+nSets = 1000
 ks =  [5, 10, 15, 20, 25, 30, 35, 40, 45]
 
 labels = ['Greedy', 'GreedyLazy', 'StocGreedy']
 
 nQueries = {}
 nQueries[labels[0]] = [nSets * a for a in ks]
-nQueries[labels[1]] = [1009, 1008, 1013, 1018, 1023, 1028, 1033, 1038, 1043]
+nQueries[labels[1]] = [2048, 2053, 2058, 2063, 2068, 2073, 2078, 2083, 2088]
 nQueries[labels[2]] = [696, 573, 538, 505, 501, 492, 498, 489, 481]
 
 values = {}
-values[labels[0]] = []
-values[labels[1]] = []
-values[labels[2]] = []
+values[labels[0]] = [256, 438, 602, 668, 756, 828, 848, 890, 925]
+values[labels[1]] = [305, 466, 648, 752, 828, 871, 900, 926, 946]
+values[labels[2]] = [[105, 149, 157, 110, 151], [171, 258, 246, 215, 275], [402, 368, 371, 384, 451], [604, 546, 542, 613, 623], [643, 637, 679, 707, 713], [806, 801, 783, 778, 749], [849, 864, 862, 817, 859], [858, 853, 871, 883, 886], [900, 917, 904, 885, 885]]
 
 
 
@@ -32,4 +32,8 @@ ylabel = r'number of value queries'
 xlabel = r'k - cardinality constraint'
 
 draw.draw_figure(ks, nQueries, labels, styles, xlabel, ylabel, (0, 10000))
+
+ylabel = r'f(S) - value of function'
+xlabel = r'k - cardinality constraint'
+draw.draw_errorbar(ks, values, labels, styles, xlabel, ylabel)
 
