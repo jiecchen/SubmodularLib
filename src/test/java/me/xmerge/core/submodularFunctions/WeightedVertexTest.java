17ac7820ff3bec84207ebe8828ac77337edfc28d
jiecchen <jiecchen@indiana.edu>
2016-03-13 16:27:55 -0400

    updated

---
+++ src/test/java/me/xmerge/core/submodularFunctions/WeightedVertexTest.java
@@ -0,0 +1,10 @@
+package me.xmerge.core.submodularFunctions;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by cjc on 3/11/16.
+ */
+public class WeightedVertexTest extends TestCase {
+
+}
---
+++ src/test/java/me/xmerge/util/DataLoaderTest.java
@@ -0,0 +1,10 @@
+package me.xmerge.util;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by cjc on 3/12/16.
+ */
+public class DataLoaderTest extends TestCase {
+
+}
