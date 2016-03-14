0d8c7ef14c1746a7ebddebe881c400311a34f53c
jiecchen <jiecchen@indiana.edu>
2016-03-14 14:23:22 -0400

    updated

--- src/main/java/me/xmerge/streaming/RandomStreamAlpha.java
+++ src/main/java/me/xmerge/streaming/RandomStreamAlpha.java
@@ -1,6 +1,9 @@
 package me.xmerge.streaming;
 
+import me.xmerge.core.Greedy;
+import me.xmerge.core.GreedyLazy;
 import me.xmerge.core.SubmodularBuffer;
+import me.xmerge.util.UtilFunctions;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -19,11 +22,13 @@ public class RandomStreamAlpha<T> implements StreamingAlgorithm<T> {
     ArrayList<T> buffer;
     double eps;
     Random rand;
+    SubmodularBuffer<T> emptyFunc;
 
     public RandomStreamAlpha(double _alpha, int _maxSize, SubmodularBuffer<T> _func, double _eps) {
         alpha = _alpha;
         maxSize = _maxSize;
         func = _func;
+        emptyFunc = (SubmodularBuffer<T>) UtilFunctions.deepClone(func);
         eps = _eps;
         rand = new Random();
         buffer = new ArrayList<>();
@@ -52,8 +57,28 @@ public class RandomStreamAlpha<T> implements StreamingAlgorithm<T> {
         }
     }
 
-    public SubmodularBuffer<T> getFunc() {
-        return func;
+
+
+    public ArrayList<T> getOptimalSolution() {
+        SubmodularBuffer<T> tmpFunc = (SubmodularBuffer<T>) UtilFunctions.deepClone(emptyFunc);
+        ArrayList<T> SS;
+        if (buffer.size() <= maxSize) {
+            SS = buffer;
+        }
+        else {
+            SS = Greedy.findOptimal(buffer, tmpFunc, maxSize);
+        }
+        if (func.getCurrentValue() > tmpFunc.eval(SS)) {
+            return func.getSolution();
+        }
+        else {
+            return SS;
+        }
+    }
+
+    public double getCurrentValue() {
+        ArrayList<T> SS = getOptimalSolution();
+        return emptyFunc.eval(SS);
     }
 
 
